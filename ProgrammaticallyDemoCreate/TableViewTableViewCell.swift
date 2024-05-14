e919435992dfc8a6dfea42a254e7c2fddf3f5055
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-05-14 12:55:20 -0700

    Commit

---
+++ ProgrammaticallyDemoCreate/TableViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewTableViewCell.swift
+//  ProgrammaticallyDemoCreate
+//
+//  Created by Arpit iOS Dev. on 14/05/24.
+//
+
+import UIKit
+
+class TableViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
